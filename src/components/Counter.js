ba1174902356e5e63ef6aa8a7fd075495218f6cf
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-08 15:22:08 +0700

    Migrate toolkit in components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,5 @@
 import classes from "./Counter.module.css";
-import {INCREMENT} from "../store/index"
+import {counterActions} from "../store/index"
 import { Component } from "react";
 import { useSelector, useDispatch, connect } from "react-redux";
 
@@ -9,19 +9,19 @@ const Counter = () => {
   const showCounter = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggle());
   };
 
   return (
