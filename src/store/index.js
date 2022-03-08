15b5d26e5aebf65850779a836b0dd661703369a9
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-08 15:46:48 +0700

    Working with multiple slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,52 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit"
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state, action) {
-            state.counter =  state.counter + action.payload;
-        },
-        toggle(state) {
-            state.showCounter = !state.showCounter;
-        },
-
-    }
-})
-
-
-
-
-const store = configureStore({
-    // reducer: {
-    //     counter: counterSlice.reducer
-    // }
-    reducer: counterSlice.reducer
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
 });
 
-export const counterActions = counterSlice.actions
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
+  // reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
